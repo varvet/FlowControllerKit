7593ccb859215e0f9e6bc0b018d4c09a7e2dacf9
Cenny Davidsson <cenny@me.com>
2018-02-15 10:17:25 +0100

    Add xcode project files

---
+++ FlowControllerKit/FlowControllerKit.h
@@ -0,0 +1,13 @@
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlowControllerKit.
+FOUNDATION_EXPORT double FlowControllerKitVersionNumber;
+
+//! Project version string for FlowControllerKit.
+FOUNDATION_EXPORT const unsigned char FlowControllerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowControllerKit/PublicHeader.h>
+
+
